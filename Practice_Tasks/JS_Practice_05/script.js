ae9768a4e58dfce7fc3fe0a7c8aeade5edeb0d97
Yash Barde <144440613+yashbarde12@users.noreply.github.com>
2023-12-26 06:24:49 +0530

    Add files via upload

---
+++ Practice_Tasks/JS_Practice_05/script.js
@@ -0,0 +1,12 @@
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+
+var length = 5;
+var width = 8;
+
+var area = calculateRectangleArea(length, width);
+
+console.log("Length: " + length + " units");
+console.log("Width: " + width + " units");
+console.log("Area: " + area + " square units");

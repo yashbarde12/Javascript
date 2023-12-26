a06fb42ea3e586cff798e5849de74da51f42f09d
Yash Barde <144440613+yashbarde12@users.noreply.github.com>
2023-12-26 06:27:29 +0530

    script.js

--- Practice_Tasks/JS_Practice_02/script.js
+++ Practice_Tasks/JS_Practice_02/script.js
@@ -1,10 +1,10 @@
 // This is a single-line comment
-var x = 5; // You can add comments at the end of a line of code
+var x = 5; // We can add comments at the end of a line of code
 
 /*
 This is a multi-line comment.
 It can span multiple lines.
-You can use it to comment out blocks of code.
+We can use it to comment out blocks of code.
 */
 
 var y = 10;

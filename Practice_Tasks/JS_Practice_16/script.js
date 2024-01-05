187eb772cbb2e7ce919fa41a7534bb1dbd965b13
Yash Barde <144440613+yashbarde12@users.noreply.github.com>
2024-01-05 21:01:00 +0530

    Add files via upload

---
+++ Practice_Tasks/JS_Practice_16/script.js
@@ -0,0 +1,10 @@
+const basicSalary = 50000;
+const allowances = 10000;
+const bonus = 5000;
+
+const grossSalary = basicSalary + allowances + bonus;
+
+console.log("Basic Salary: $" + basicSalary);
+console.log("Allowances: $" + allowances);
+console.log("Bonus: $" + bonus);
+console.log("Gross Salary: $" + grossSalary);

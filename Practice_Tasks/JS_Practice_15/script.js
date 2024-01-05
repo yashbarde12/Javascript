2edaaa2d9dc1b6c4b2ee653f2bdcdeaecd6b1d1d
Yash Barde <144440613+yashbarde12@users.noreply.github.com>
2024-01-05 20:58:59 +0530

    Add files via upload

---
+++ Practice_Tasks/JS_Practice_15/script.js
@@ -0,0 +1,18 @@
+function calculateBMI(weight, height) {
+
+    var heightInMeters = height / 100;
+
+    var bmi = weight / (heightInMeters * heightInMeters);
+
+    return bmi;
+}
+
+var weight = 70;
+var height = 175;
+
+var bmiResult = calculateBMI(weight, height);
+
+console.log("Weight: " + weight + " kg");
+console.log("Height: " + height + " cm");
+console.log("BMI: " + bmiResult.toFixed(2)); 
+// Display BMI with two decimal places
